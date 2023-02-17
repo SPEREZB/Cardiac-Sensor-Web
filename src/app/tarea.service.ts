23231ade6a32cb65b5770322de9e06b4890de78d
SebasPBR18 <sperezb@uteq.edu.ec>
2023-02-17 03:38:31 -0500

    uso de apis alojadas en Railway

--- src/app/tarea.service.ts
+++ src/app/tarea.service.ts
@@ -13,7 +13,10 @@ export interface Message{
 
 export class TareaService {  
   
-  private api= 'http://localhost:8080/usuario';
+  private api= 'https://sensortdd4iots-production.up.railway.app/usuario';
+  private apipulso= 'https://sensortdd4iots-production.up.railway.app/frecuenciacardiaca/all';
+  private apipersona= 'https://sensortdd4iots-production.up.railway.app/persona';
+  private apipaciente= 'https://sensortdd4iots-production.up.railway.app/paciente';
   constructor(
     private http:HttpClient
   ) { }
@@ -21,4 +24,20 @@ export class TareaService {
   public gettarea():Observable<any>{
     return this.http.get(this.api);
  }
+
+ public getpulso():Observable<any>{
+  return this.http.get(this.apipulso);
+}
+ 
+//agreg
+ public postusuario(body:any):Observable<any>{
+  return this.http.post(this.api,body);
+}
+public postpersona(body:any):Observable<any>{
+  return this.http.post(this.apipersona,body);
+}
+public postpaciente(body:any):Observable<any>{
+  return this.http.post(this.apipaciente,body);
+}
+
 }
